698e2f8ff8391e76e759ec6d2e34c8b3f9336c4e
saiful63 <saifulislamraihan64@gmail.com>
2023-02-25 08:12:23 +0600

    pattern 1

---
+++ pattern.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+int n;
+int i=0;
+cin>>n;
+while(i<n){
+int j=1;
+
+while(j<=n){
+
+cout<<j<<" ";
+j=j+1;
+
+}
+cout<<endl;
+
+
+i++;
+
+}
+
+
+
+}
