adfa9bba79dc9731ba963d258209da5ede4efa67
saiful63 <saifulislamraihan64@gmail.com>
2023-02-25 09:04:29 +0600

    pattern 3

---
+++ pattern  2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+int n;
+int i=0;
+cin>>n;
+while(i<n){
+int j=n;
+
+while(j>=1){
+
+cout<<j<<" ";
+j=j-1;
+
+}
+cout<<endl;
+
+
+i++;
+
+}
+
+
+
+}
---
+++ pattern 3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+int n;
+int i=0;
+cin>>n;
+
+while(i<1){
+int j=1;
+int r=0;
+while(j<=n*n){
+
+cout<<j<<" ";
+j=j+1;
+r=r+1;
+if(r%3==0){
+cout<<endl;
+}
+
+}
+
+
+
+
+i++;
+
+}
+
+
+
+}
